9988f72decac9bf8a047bc2d4f468ae291608ca7
stone <stone@stones-MBP.local>
2022-08-09 23:49:44 +0800

    update download geo data file script

--- Dockerfile
+++ Dockerfile
@@ -19,8 +19,8 @@ RUN apk add --no-cache ca-certificates \
 	&& mkdir /etc/mosdns
 ADD entrypoint.sh /entrypoint.sh
 ADD config.yaml /config.yaml
-ADD https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat /geoip.dat
-ADD https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat /geosite.dat
+ADD /geodata/geoip.dat
+ADD /geodata/geosite.dat
 VOLUME /etc/mosdns
 EXPOSE 53/udp 53/tcp
 RUN chmod +x /entrypoint.sh
--- update-geodata.sh
+++ update-geodata.sh
@@ -1,4 +1,10 @@
 mkdir -p ./geodata
-curl -LJ https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat -o ./geodata/geoip.dat
-curl -LJ https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat.sha256sum -o ./geodata/geoip.dat.sha256sum
-curl -LJ https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat.sha256sum -o ./geodata/geosite.dat.sha256sum
+cd geodata
+curl -LJO https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+curl -LJO https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat.sha256sum
+curl -LJO https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
+curl -LJO https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat.sha256sum
+
+shasum -a 256 -c geoip.dat.sha256sum
+shasum -a 256 -c geosite.dat.sha256sum
+cd ..
